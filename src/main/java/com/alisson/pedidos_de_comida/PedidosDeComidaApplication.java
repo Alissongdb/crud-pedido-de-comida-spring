9d4e467ebe19cd2a96edea5f85b82935bbf3a226
Alissongdb <alisson.gabriel5@hotmail.com>
2025-03-25 23:05:50 -0300

    feat/commit das configs iniciais

---
+++ src/main/java/com/alisson/pedidos_de_comida/PedidosDeComidaApplication.java
@@ -0,0 +1,13 @@
+package com.alisson.pedidos_de_comida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosDeComidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosDeComidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alisson/pedidos_de_comida/PedidosDeComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisson.pedidos_de_comida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosDeComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
